33d792ba1b01d41ebf035d824f657d9b0b3a7434
Dmitri <DRU@popesco.com>
2014-05-16 21:30:29 +0300

    readme

--- libs/OmlManager/ORM/Query/Expression/Expression.php
+++ libs/OmlManager/ORM/Query/Expression/Expression.php
@@ -48,7 +48,7 @@ class Expression implements ExpressionInterface {
 	private $prepareStatement = array();
 
 	public function __construct($expression = '1=1') {
-		if ( $expression !== '1=1') {
+		if ( $expression === '1=1') {
 
 			$this->expressions[] = $expression;
 		}
