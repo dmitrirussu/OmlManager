14a714329a72e5e4f1e02bc9ff87d2276bb3bcc0
Dmitri <DRU@popesco.com>
2014-09-16 15:50:41 +0300

    was improved

---
+++ libs/OmlManager/ORM/Models/ReaderException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Dumitru Russu.
+ * Date: 28.06.2014
+ * Time: 00:31
+ * OmlManager\ORM\Models${NAME} 
+ */
+
+namespace OmlManager\ORM\Models;
+
+
+class ReaderException extends \Exception {
+
+} 
